b3ed31a5a8114f1de00a94a7fac90f92bee452f3
norwaylua <hilosarry77@gmail.com>
2025-08-30 02:04:52 +0700

    Update Fish.lua

--- Fish.lua
+++ Fish.lua
@@ -3,7 +3,7 @@
 --_G.NoclipEnabled = true  -- Toggle noclip
 --_G.MakeSafeBase = true   -- Toggle safe platform
 --getgenv().firetools = true
---_G.Auto_Spins_AutoclaimGitf = true
+--_G.Auto_Spins_AutoclaimGitf = true -- fixed laters due error
 -- // Services
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
@@ -79,8 +79,25 @@ task.spawn(function()
 end)
 
 ------
--- auto spins and auto claim Gitfs 
+-- auto spins and auto claim Gitfs -- kinda broken
 ------
+
+--[[
+while _G.Auto_Spins_AutoclaimGitf do
+    for gift = 1, 12 do
+        local args = {
+            "Gift" .. gift
+        }
+        game:GetService("ReplicatedStorage"):WaitForChild("Signals"):WaitForChild("ClaimGift"):FireServer(unpack(args))
+        task.wait(0.2)
+    end
+
+    local spinSignal = game:GetService("ReplicatedStorage"):WaitForChild("Signals"):WaitForChild("Spin")
+    pcall(function()
+        spinSignal:InvokeServer()
+    end)
+end
+]]
 
 -------------------------------------------------
 -- Safe Platform
